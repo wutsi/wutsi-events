7cc247d410eaed97a5ea6deb1c40b29e852fd6a0
wutsi <herve.tchepannou@wutsi.com>
2022-11-26 15:34:48 -0500

    add events

---
+++ src/main/kotlin/com/wutsi/event/EventURN.kt
@@ -0,0 +1,18 @@
+package com.wutsi.event
+
+enum class EventURN(val urn: String) {
+    BUSINESS_CREATED("urn:wutsi:event:business-created"),
+    BUSINESS_SUSPENDED("urn:wutsi:event:business-suspended"),
+
+    BUSINESS_ACCOUNT_ENABLED("urn:wutsi:event:business-account-enabled"),
+    BUSINESS_ACCOUNT_DISABLED("urn:wutsi:event:business-account-disabled"),
+
+    MEMBER_ATTRIBUTE_UPDATED("urn:wutsi:event:member-attribute-updated"),
+    MEMBER_DELETED("urn:wutsi:event:member-deleted"),
+    MEMBER_REGISTERED("urn:wutsi:event:member-registered"),
+
+    STORE_ENABLED("urn:wutsi:event:store-enabled"),
+    STORE_SUSPENDED("urn:wutsi:event:store-suspended"),
+
+    PAYMENT_METHOD_ADDED("urn:wutsi:event:payment-method-added")
+}
---
+++ src/main/kotlin/com/wutsi/event/StoreEventPayload.kt
@@ -0,0 +1,6 @@
+package com.wutsi.event
+
+data class StoreEventPayload(
+    val accountId: Long = -1,
+    val storeId: Long = -1
+)
