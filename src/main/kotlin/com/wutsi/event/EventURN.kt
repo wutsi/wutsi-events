ff3421ecb25bd28b1f789229b0bf5b4a8bc5aaba
wutsi <herve.tchepannou@wutsi.com>
2022-12-07 13:21:59 -0500

    update events

--- src/main/kotlin/com/wutsi/event/EventURN.kt
+++ src/main/kotlin/com/wutsi/event/EventURN.kt
@@ -2,10 +2,10 @@ package com.wutsi.event
 
 enum class EventURN(val urn: String) {
     BUSINESS_CREATED("urn:wutsi:event:business-created"),
-    BUSINESS_SUSPENDED("urn:wutsi:event:business-suspended"),
+    BUSINESS_DEACTIVATED("urn:wutsi:event:business-deactivated"),
 
-    BUSINESS_ACCOUNT_ENABLED("urn:wutsi:event:business-account-enabled"),
-    BUSINESS_ACCOUNT_DISABLED("urn:wutsi:event:business-account-disabled"),
+    BUSINESS_ACCOUNT_ACTIVATED("urn:wutsi:event:business-account-deactivated"),
+    BUSINESS_ACCOUNT_DEACTIVATED("urn:wutsi:event:business-account-deactivated"),
 
     MEMBER_ATTRIBUTE_UPDATED("urn:wutsi:event:member-attribute-updated"),
     MEMBER_DELETED("urn:wutsi:event:member-deleted"),
@@ -13,8 +13,8 @@ enum class EventURN(val urn: String) {
 
     ORDER_EXPIRED("urn:wutsi:event:order-expired"),
 
-    STORE_ENABLED("urn:wutsi:event:store-enabled"),
-    STORE_SUSPENDED("urn:wutsi:event:store-suspended"),
+    STORE_ACTIVATED("urn:wutsi:event:store-activated"),
+    STORE_DEACTIVATED("urn:wutsi:event:store-deactivated"),
 
     PAYMENT_METHOD_ADDED("urn:wutsi:event:payment-method-added")
 }
