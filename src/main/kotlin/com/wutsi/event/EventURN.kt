3242b6402b2045111440961bb04d40bf8c8deee6
wutsi <herve.tchepannou@wutsi.com>
2022-12-09 13:46:18 -0500

    add order events

--- src/main/kotlin/com/wutsi/event/EventURN.kt
+++ src/main/kotlin/com/wutsi/event/EventURN.kt
@@ -9,6 +9,10 @@ enum class EventURN(val urn: String) {
     MEMBER_DELETED("urn:wutsi:event:member-deleted"),
     MEMBER_REGISTERED("urn:wutsi:event:member-registered"),
 
+    ORDER_OPENED("urn:wutsi:event:order-opened"),
+    ORDER_STARTED("urn:wutsi:event:order-started"),
+    ORDER_COMPLETED("urn:wutsi:event:order-completed"),
+    ORDER_CANCELLED("urn:wutsi:event:order-cancelled"),
     ORDER_EXPIRED("urn:wutsi:event:order-expired"),
 
     STORE_ACTIVATED("urn:wutsi:event:store-activated"),
